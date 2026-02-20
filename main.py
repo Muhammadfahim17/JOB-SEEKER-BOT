4c47e83ca61919894ff0f13173ad58fb4ccb0e47
Muhammadfahim17 <fahimbodurov@gmail.com>
2026-02-20 17:41:13 +0500

    добавление изменения файла Readme в основную част кода

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 
-# ВАЖНО: для Windows нужно использовать SelectorEventLoop
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
@@ -13,12 +12,10 @@ from config import config
 from database import engine, Base
 from handlers import router
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def on_startup():
-    # Создаем таблицы
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     logger.info("✅ База данных подключена")
@@ -29,7 +26,6 @@ async def main():
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
     
-    # Подключаем роутер
     dp.include_router(router)
     
     dp.startup.register(on_startup)
